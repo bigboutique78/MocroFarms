960dfd6d2ca7cd77bd6dd3a902a1e80b2d5db305
bigboutique78 <zertybr@gmail.com>
2026-02-23 13:26:34 +0100

    Initialize script.js with Telegram WebApp functions

---
+++ script.js
@@ -0,0 +1,9 @@
+const tg = window.Telegram.WebApp;
+
+tg.expand();
+tg.setHeaderColor("#000000");
+tg.setBackgroundColor("#000000");
+
+function contact() {
+  tg.showAlert("Redirection vers le contact...");
+}
